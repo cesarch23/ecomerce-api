baec3384e12e73528fe52a3f67366f81d67fc256
cesar <c21choccata@gmail.com>
2024-01-31 17:57:21 -0500

    Add SaleDetails entity

---
+++ src/main/java/com/edu/Ecomerce/entities/SaleDetails.java
@@ -0,0 +1,34 @@
+package com.edu.Ecomerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "sale_details")
+public class SaleDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sale_detail_id")
+    private int id;
+    @Column(name = "sale_detail_amount")
+    private int amount;
+    @Column(name = "sale_detail_price")
+    private double price;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "sale_id", referencedColumnName ="sale_id", foreignKey = @ForeignKey(name = "fK_sale_details_sale"))
+    private  Sale sale;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", referencedColumnName ="product_id", foreignKey = @ForeignKey(name = "fk_sale_details_products"))
+    private Product product;
+
+    public  SaleDetails(){}
+
+    public SaleDetails(int id, int amount, double price, Sale sale, Product product) {
+        this.id = id;
+        this.amount = amount;
+        this.price = price;
+        this.sale = sale;
+        this.product = product;
+    }
+}
