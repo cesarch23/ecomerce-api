ddec5ff18b1c4cab84288a70be4bc657bb02b30e
cesar <c21choccata@gmail.com>
2024-01-31 17:38:21 -0500

    Add Product entity

---
+++ src/main/java/com/edu/Ecomerce/entities/Product.java
@@ -0,0 +1,31 @@
+package com.edu.Ecomerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int id;
+    @Column(name = "product_description")
+    private String description;
+    @Column(name = "product_code")
+    private String code;
+    @Column(name = "product_stock")
+    private int stock;
+    @Column(name = "product_price")
+    private double price;
+
+    public  Product(){}
+    public Product(int id, String description, String code, int stock, double price) {
+        this.id = id;
+        this.description = description;
+        this.code = code;
+        this.stock = stock;
+        this.price = price;
+    }
+}
