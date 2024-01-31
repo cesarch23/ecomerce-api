b568296700a584672fe034a55a1b13c2d760ebcf
cesar <c21choccata@gmail.com>
2024-01-31 17:14:05 -0500

    Add client entity

--- src/main/java/com/edu/Ecomerce/Cliente.java
+++
@@ -1,6 +0,0 @@
-package com.edu.Ecomerce;
-
-public class Cliente
-{
-    //
-}
---
+++ src/main/java/com/edu/Ecomerce/entities/Client.java
@@ -0,0 +1,31 @@
+package com.edu.Ecomerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Column(name = "client_id")
+    private int id;
+    @Column(name = "client_name" , length = 100)
+    private String name;
+    @Column(name = "client_lastname" , length = 250)
+    private String lastname;
+    @Column(name = "client_dni" , length = 8)
+    private String dni;
+
+    public Client() {
+    }
+    public Client(int id, String name, String lastname, String dni) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.dni = dni;
+    }
+
+
+}
